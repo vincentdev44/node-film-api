3a865c0f84ed01a8b292381e56cda6d213242ad4
vincent cailliau <vincentcailliaudev@gmail.com>
2022-06-20 17:54:03 +0200

    pour le deploiment avec DB

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -4,21 +4,35 @@ const UserModel = require('../models/user')
 const films = require('./mock-film')
 const bcrypt = require('bcrypt')
 
-const sequelize = new Sequelize('myflix', 'root', '', {
-    host: 'localhost',
-    dialect: 'mariadb',
-    dialectOptions: {
-        timezone: 'Etc/GMT-2',
-    },
-    logging: false
-})
+let sequelize
+
+if (process.env.NODE_ENV === 'production') {
+    sequelize = new Sequelize('tzi4z67jwly6j1vk', 'x2dwurheo7rwibm1', 's0sp6zenun7xbxhy', {
+        host: 'j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+        dialect: 'mariadb',
+        dialectOptions: {
+            timezone: 'Etc/GMT-2',
+        },
+        logging: true
+    })
+} else {
+    sequelize = new Sequelize('myflix', 'root', '', {
+        host: 'localhost',
+        dialect: 'mariadb',
+        dialectOptions: {
+            timezone: 'Etc/GMT-2',
+        },
+        logging: true
+    })
+}
 
 const Film = FilmModel(sequelize, DataTypes)
 const User = UserModel(sequelize, DataTypes)
 
 
 const initDb = () => {
-    return sequelize.sync({ force: true }).then(_ => {
+    return sequelize.sync().then(_ => {
+        console.log('INIT DB')
         films.map(film => {
             Film.create({
                 name: film.name,
